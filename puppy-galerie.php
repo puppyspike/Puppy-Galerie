9e093e59498e8149e335825d1bbfbf1b68f0cd29
Puppy Spike <github@felixlimburger.de>
2022-09-13 13:29:01 +0000

    first commit

--- puppy-galerie.php
+++ puppy-galerie.php
@@ -21,7 +21,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 );
 
 //Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('stable-branch-name');
+$myUpdateChecker->setBranch('main');
 
 //Optional: If you're using a private repository, specify the access token like this:
 $myUpdateChecker->setAuthentication('your-token-here');
